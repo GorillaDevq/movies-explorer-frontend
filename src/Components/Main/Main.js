fe8294d09fd9f4966d90b0689c3296fcb5b25161
Gorilla Dev <gorilladevq@gmail.com>
2023-06-10 19:41:00 +0300

    section promo

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,4 +1,5 @@
-import './Main.css'
+import './Main.css';
+import studentPath from '../../images/student.png';
 
 export default function Main() {
   return (
@@ -50,6 +51,27 @@ export default function Main() {
             <li className='techs__item'>Express.js</li>
             <li className='techs__item'>mongoDB</li>
           </ul>
+        </div>
+      </article>
+      <article className='about-me'>
+        <h2 className='article-title'>Студент</h2>
+        <div className='about-me__columns'>
+          <ul className='about-me__info'>
+            <li className='about-me__item'>
+              <h3 className='about-me__title'>Виталий</h3>
+            </li>
+            <li className='about-me__item'>
+              <p className='about-me__subtitle'>Фронтенд-разработчик, 30 лет</p>
+            </li>
+            <li className='about-me__item'>
+              <p className='about-me__paragraph'>
+                Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь. 
+                Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». 
+                После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.
+              </p>
+            </li>
+          </ul>
+          <img className='about-me__image' alt='Студент' src={studentPath}/>
         </div>
       </article>
     </main>
